fd58699efe2c1d1bf41382eb2cde3cead00707c8
fililou <cardland@msn.com>
2023-06-14 02:33:59 -0400

    Added zooming!

--- life.js
+++ life.js
@@ -4,9 +4,12 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
 
 const MAX_MOVE_FOR_CLICK = 10;
-const GRID_WIDTH = 1000;
-const GRID_HEIGHT = 1000;
+const GRID_WIDTH = 500;
+const GRID_HEIGHT = 500;
 const SIMULATION_RATE = 4;
+const SQUARE_SIZE = 20;
+const MIN_ZOOM = 0.2;
+const MAX_ZOOM = 2;
 
 let grid = []
 for (let x = 0; x < GRID_WIDTH; x++) {
@@ -16,11 +19,11 @@ for (let x = 0; x < GRID_WIDTH; x++) {
     }
     grid.push(a)
 }
-grid[1][1] = true;
-console.log(grid);
 
-let cameraX = 5000;
-let cameraY = 5000;
+let cameraX = GRID_WIDTH / 2 * SQUARE_SIZE - canvas.width / 2;
+let cameraY = GRID_HEIGHT / 2 * SQUARE_SIZE - canvas.height / 2;
+let cameraZoom = 1;
+let squareSize = SQUARE_SIZE;
 
 let mouseX = 0;
 let mouseY = 0;
@@ -54,11 +57,9 @@ function mouseMove(e) {
 function mouseDown(e) {
     mousePressed = true;
     mouseMovement = 0;
-
 }
 function mouseUp(e) {
     mousePressed = false;
-    console.log("moved " + mouseMovement);
     if (mouseMovement <= MAX_MOVE_FOR_CLICK) {
         click()
     }
@@ -66,36 +67,56 @@ function mouseUp(e) {
 
 function click() {
     // Toggles the square
-    let x = Math.floor((mouseX + cameraX) / 10);
-    let y = Math.floor((mouseY + cameraY) / 10);
+    let x = Math.floor((mouseX + cameraX) / squareSize);
+    let y = Math.floor((mouseY + cameraY) / squareSize);
     grid[x][y] = !grid[x][y];
 
     // Stops simulating
     simulating = false;
 
-    console.log("toggled " + x + " " + y);
     requestAnimationFrame(draw);
+}
+
+function scroll(e) {
+    // Check the scroll direction
+    const delta = e.deltaY > 0 ? 0.1 : -0.1;
+
+    let newCameraZoom = Math.max(Math.min(cameraZoom + delta, MAX_ZOOM), MIN_ZOOM);
+
+    // Adjust camera FIX THIS SO IT STAYS AT THE SAME SPOT RELATIVE TO THE CAMERA
+    cameraX = (cameraX + canvas.width / 2) / (GRID_WIDTH * SQUARE_SIZE / cameraZoom) * (GRID_WIDTH * SQUARE_SIZE / newCameraZoom) - canvas.width / 2
+    cameraY = (cameraY + canvas.height / 2) / (GRID_HEIGHT * SQUARE_SIZE / cameraZoom) * (GRID_HEIGHT * SQUARE_SIZE / newCameraZoom) - canvas.height / 2
+
+    // Update the zoom level
+    cameraZoom = newCameraZoom;
+    squareSize = SQUARE_SIZE / cameraZoom;
+
+    requestAnimationFrame(draw);
+    e.preventDefault();
 }
 
 function draw() {
     // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
+
     // Camera adjustments
     context.translate(-cameraX, -cameraY);
 
-    // Set the fill style
+    // Fill grid bg
+    context.fillStyle = "rgb(21, 32, 43)";
+    context.fillRect(0, 0, GRID_WIDTH * squareSize, GRID_HEIGHT * squareSize);
+    // Fill the squares
     context.fillStyle = '#ddd';
-
     for (let x = 0; x < GRID_WIDTH; x++) {
         for (let y = 0; y < GRID_HEIGHT; y++) {
             if (grid[x][y]) {
-                context.fillRect(x * 10, y * 10, 10, 10);
+                context.fillRect(x * squareSize, y * squareSize, squareSize + 1, squareSize + 1);
             }
         }
     }
 
     // Reset camera adjustments
-    context.translate(cameraX, cameraY);
+    context.setTransform(1, 0, 0, 1, 0, 0);
 }
 
 function keyUp(e) {
@@ -117,7 +138,6 @@ function updateLoop() {
 }
 
 function update() {
-    console.log("update")
     let newGrid = [];
     for (let x = 0; x < GRID_WIDTH; x++) {
         let a = []
@@ -152,7 +172,10 @@ function sleep(ms) {
 
 canvas.addEventListener('mousemove', mouseMove);
 canvas.addEventListener('mousedown', mouseDown);
+canvas.addEventListener('dragstart', mouseDown);
 canvas.addEventListener('mouseup', mouseUp);
+canvas.addEventListener('dragend', mouseUp);
 document.addEventListener("keyup", keyUp);
+canvas.addEventListener('wheel', scroll);
 
 requestAnimationFrame(draw);
